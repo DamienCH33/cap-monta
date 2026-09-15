aecfe73dd107dc913dd6277128b83aa74b7aa800
Damien Chauveau <damienchauveau@hotmail.fr>
2026-09-15 15:09:17 +0200

    feat(web): formulaire de recherche par dates et voyageurs

--- web/src/app/pages/search/search.ts
+++ web/src/app/pages/search/search.ts
@@ -1,9 +1,11 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { AccommodationService } from '../../core/services/accommodation';
 import { Accommodation } from '../../core/models/accommodation';
+import { FormsModule } from '@angular/forms';
+import { SearchCriteria } from '../../core/models/search-criteria';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'cm-search',
   styleUrl: './search.scss',
   templateUrl: './search.html',
@@ -12,9 +14,23 @@ export class Search implements OnInit {
   private readonly accommodationService = inject(AccommodationService);
 
   readonly results = signal<Accommodation[]>([]);
+  readonly arrival = signal('');
+  readonly departure = signal('');
+  readonly guests = signal(2);
 
   ngOnInit(): void {
-    this.accommodationService.search({}).subscribe((accommodations) => {
+    this.search();
+  }
+
+  search(): void {
+    const criteria: SearchCriteria = { guests: this.guests() };
+
+    if (this.arrival() && this.departure()) {
+      criteria.arrival = this.arrival();
+      criteria.departure = this.departure();
+    }
+
+    this.accommodationService.search(criteria).subscribe((accommodations) => {
       this.results.set(accommodations);
     });
   }
