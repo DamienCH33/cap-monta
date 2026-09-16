f52178156a9ea62905ab60ed3b9d6fb76f39c134
Damien Chauveau <damienchauveau@hotmail.fr>
2026-09-16 10:41:54 +0200

    feat(web): en-tête du site et conteneur de page

---
+++ web/src/app/layout/header/header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'cm-header',
+  imports: [RouterLink],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {}
